4320282808c1d54eb59c3f3566957a7e1c7291c9
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-09-16 16:44:17 +0100

    Initial commit in palindrome

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+// A function to find the palindrome of word
+
+
+function palindrome () {
+    let pali = ["m", "u", "d"]
+    let pali2 = pali.reverse()
+    return pali2
+}
+
+result = palindrome();
+console.log(result)
